9cbb3e77543af90d5ab7b7760f23bb8799875195
Skeeven Men <skeeven.men@efrei.net>
2022-03-16 16:30:28 +0100

    commit v5

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+  function ScrollRight (akame) {
+    document.getElementById(akame).scrollLeft += 100;
+    
+ 
+  };
+  function ScrollLeft (akame) {
+    document.getElementById(akame).scrollLeft -= 100;
+    
+  };
+
